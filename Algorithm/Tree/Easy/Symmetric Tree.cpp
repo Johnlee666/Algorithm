581a88589dea17dc8b80a6504a3c1955302ece50
Johnlee666 <895471139@qq.com>
2019-02-27 23:53:16 +0800

    Symmetric Tree

--- Algorithm/Tree/Easy/Symmetric Tree.cpp
+++ Algorithm/Tree/Easy/Symmetric Tree.cpp
@@ -7,3 +7,51 @@
 //
 
 #include <stdio.h>
+#include "Header.h"
+//Recursive
+//class Solution {
+//public:
+//    bool isSymmetric(TreeNode* root) {
+//        return isMirror(root, root);
+//    }
+//    bool isMirror(TreeNode* left, TreeNode* right) {
+//        if (left == nullptr && right == nullptr) {
+//            return true;
+//        }
+//        if (!left || !right) {
+//            return false;
+//        }
+//        return isMirror(left->left, right->right) && isMirror(left->right, right->left) && left->val == right->val;
+//    }
+//};
+
+
+//Iterative
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* t1 = q.front();
+            q.pop();
+            TreeNode* t2 = q.front();
+            q.pop();
+            if (!t1 && !t2) {
+                continue;
+            }
+            if (!t1 || !t2) {
+                return false;
+            }
+            if (t1->val != t2->val) {
+                return false;
+            }
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
+    }
+};
