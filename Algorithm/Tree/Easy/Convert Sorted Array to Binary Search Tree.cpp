0c2f9032fb25e7784f6b9208f3d088ade4114356
Johnlee666 <895471139@qq.com>
2019-03-01 23:54:20 +0800

    tree

---
+++ Algorithm/Tree/Easy/Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,26 @@
+//
+//  Convert Sorted Array to Binary Search Tree.cpp
+//  Algorithm
+//
+//  Created by 李展 on 2019/3/1.
+//  Copyright © 2019 JohnLee. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Header.h"
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return createBST(nums, 0, nums.size()-1);
+    }
+    TreeNode *createBST(vector<int>& nums, int l, int r) {
+        if (l <= r) {
+            int m = (l+r)/2;
+            TreeNode *res = new TreeNode(nums[m]);
+            res->left = createBST(nums, l, m-1);
+            res->right = createBST(nums, m+1, r);
+            return res;
+        }
+        return nullptr;
+    }
+};
--- Algorithm/Tree/Easy/Subtree of Another Tree.cpp
+++ Algorithm/Tree/Easy/Subtree of Another Tree.cpp
@@ -7,3 +7,54 @@
 //
 
 #include <stdio.h>
+#include "Header.h"
+
+//Using Preorder Traversal
+//class Solution {
+//public:
+//    bool isSubtree(TreeNode* s, TreeNode* t) {
+//        string tree1 = preorder(s, true);
+//        string tree2 = preorder(t, true);
+//        return tree1.find(tree2) != string::npos;
+//    }
+//    string preorder(TreeNode *t, bool left) {
+//        if (t == nullptr) {
+//            if (left) {
+//                return "lnull";
+//            } else {
+//                return "rnull";
+//            }
+//        }
+//        return "#" + to_string(t->val) + preorder(t->left, true) + preorder(t->right, false);
+//    }
+//};
+
+By Comparison of Nodes
+class Solution {
+public:
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if(!s){
+            if (!t) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (isSame(s, t)) {
+            return true;
+        }
+        return isSubtree(s->left, t) || isSubtree(s->right, t);
+    }
+    bool isSame(TreeNode *s, TreeNode *t){
+        if (!s && !t) {
+            return true;
+        }
+        else if (!s || !t) {
+            return false;
+        }
+        if (s->val == t->val) {
+            return isSame(s->left, t->left) && isSame(s->right, t->right);
+        }
+        return false;
+    }
+};
