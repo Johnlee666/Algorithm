50f69a638a2d52ca40aa1444bee47c051c2049c3
Johnlee666 <895471139@qq.com>
2019-03-02 23:48:57 +0800

    To Lower Case

---
+++ Algorithm/String/Easy/To Lower Case.cpp
@@ -0,0 +1,21 @@
+//
+//  To Lower Case.cpp
+//  Algorithm
+//
+//  Created by 李展 on 2019/3/2.
+//  Copyright © 2019 JohnLee. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Header.h"
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] >= 'a' && str[i] <= 'z') {
+                str[i] -= 32;
+            }
+        }
+        return str;
+    }
+};
