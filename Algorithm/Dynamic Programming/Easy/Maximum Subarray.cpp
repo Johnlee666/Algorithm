ec7009f07243eb03e69a8c19f1bca52698414740
Johnlee666 <895471139@qq.com>
2019-03-03 23:41:58 +0800

    Maximum Subarray

---
+++ Algorithm/Dynamic Programming/Easy/Maximum Subarray.cpp
@@ -0,0 +1,26 @@
+//
+//  Maximum Subarray.cpp
+//  Algorithm
+//
+//  Created by 李展 on 2019/3/3.
+//  Copyright © 2019 JohnLee. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Header.h"
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int m = INT_MIN;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (sum < 0) {
+                sum = 0;
+            }
+            sum += nums[i];
+            m = max(sum,m);
+        }
+        return m;
+    }
+};
+
