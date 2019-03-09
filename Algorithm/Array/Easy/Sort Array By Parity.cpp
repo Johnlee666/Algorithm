68d8a5c443b7e58fdc5c27dfb9d221964d606367
Johnlee666 <895471139@qq.com>
2019-03-10 00:04:54 +0800

    array

--- Algorithm/Array/Easy/Sort Array By Parity.cpp
+++ Algorithm/Array/Easy/Sort Array By Parity.cpp
@@ -7,3 +7,14 @@
 //
 
 #include <stdio.h>
+class Solution {
+private:
+    static bool compare(const int &i, const int &j) {
+        return j % 2 > i % 2;
+    }
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        sort(A.begin(), A.end(), compare);
+        return A;
+    }
+};
